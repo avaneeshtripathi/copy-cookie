87bedc11b6b0d927a32a381e7d423831c2ec6ea8
avaneeshtripathi <avaneeshtripathi93@gmail.com>
2020-06-27 22:44:46 +0530

    feat: performance improvements and callbacks to keep removal and setting in sync

--- popup.js
+++ popup.js
@@ -2,98 +2,112 @@
 const onCopyButtonClick = () => {
     chrome.tabs.query(
         {
-            status: 'complete',
-            windowId: chrome.windows.WINDOW_ID_CURRENT,
+            currentWindow: true,
             active: true,
         },
         tab => {
             chrome.cookies.getAll({ url: tab[0].url }, cookie => {
-                localStorage.aviCookieData = JSON.stringify(cookie);
+                localStorage.copyCookieData = btoa(JSON.stringify(cookie));
+                setTimeout(() => handlePopupUI('copy'), 50); // Buffer time to set local storage
             });
         },
     );
-    setTimeout(() => handlePopupUI('copy'), 200);
+};
+
+const removeCookies = (cookies, index, url, callback) => {
+    if (!cookies[index]) return callback();
+
+    chrome.cookies.remove({
+        url: url + cookies[index].path,
+        name: cookies[index].name,
+    }, () => removeCookies(cookies, index + 1, url, callback));
+};
+
+const setNewCookies = (cookies, index, url, callback) => {
+    if (!cookies[index]) return callback();
+
+    const { name, value, path, domain } = cookies[index];
+    try {
+        chrome.cookies.set({
+            url,
+            name,
+            value,
+            path,
+            domain,
+        }, () => setNewCookies(cookies, index + 1, url, callback));
+    } catch (e) {
+        console.warn(`There was an error setting the cookies: ${error}`);
+    }
 };
 
 // When the popup Paste Button is clicked
 const onPasteButtonClick = () => {
-    const aviCookieData = localStorage.aviCookieData
-        ? JSON.parse(localStorage.aviCookieData)
-        : null;
-    if (!aviCookieData)
+    let copyCookieData;
+    try {
+        copyCookieData = localStorage.copyCookieData
+            ? JSON.parse(atob(localStorage.copyCookieData))
+            : null;
+    } catch (e) {
+        return alert('Error parsing cookies. Please try again.');
+    }
+
+    if (!copyCookieData)
         return alert('Oh Man! You need to copy the cookies first.');
 
     let domain = document.getElementById('domainInput').value.trim();
     if (!domain) domain = 'localhost';
 
-    let validTabUrl = true;
+    // ENABLE LOADER
+    const spinner = document.getElementById('loader');
+    spinner.setAttribute('style', 'display: flex;');
 
     chrome.tabs.query(
         {
-            status: 'complete',
-            windowId: chrome.windows.WINDOW_ID_CURRENT,
+            currentWindow: true,
             active: true,
         },
         tab => {
-            if (tab[0].url) {
-                chrome.cookies.getAll({ url: tab[0].url }, cookies => {
-                    for (let i = 0; i < cookies.length; i++) {
-                        chrome.cookies.remove({
-                            url: tab[0].url + cookies[i].path,
-                            name: cookies[i].name,
-                        });
-                    }
-                });
-                aviCookieData.forEach(({ name, value, path }, index) => {
-                    try {
-                        chrome.cookies.set({
-                            url: tab[0].url,
-                            name,
-                            value,
-                            path,
-                            domain,
-                        });
-                    } catch (error) {
-                        console.error(`There was an error: ${error}`);
-                    }
-                });
-            } else {
-                validTabUrl = false;
+            if (!tab?.[0]?.url) {
+                spinner.setAttribute('style', 'display: none;');
                 return alert('Tab with invalid URL. Are you kidding me ???');
             }
+            
+            chrome.cookies.getAll({ url: tab[0].url }, cookies => {
+                removeCookies(cookies, 0, tab[0].url, () => {
+                    setNewCookies(copyCookieData, 0, tab[0].url, () => {
+                        spinner.setAttribute('style', 'display: none;');
+                        return onResetButtonClick('paste');
+                    });
+                });
+            });
         },
     );
-    setTimeout(() => {
-        if (validTabUrl) onResetButtonClick('paste');
-    }, 200);
 };
 
 // When the popup Reset Button is clicked
 const onResetButtonClick = action => {
-    localStorage.removeItem('aviCookieData');
+    localStorage.removeItem('copyCookieData');
     handlePopupUI(action);
 };
 
 const handlePopupUI = action => {
-    const aviCookieData = localStorage.aviCookieData
-        ? JSON.parse(localStorage.aviCookieData)
-        : null;
+    const copyCookieData = localStorage.copyCookieData
     const containerElement = document.getElementById('container');
     containerElement.setAttribute('class', '');
-    if (aviCookieData) {
+    if (copyCookieData) {
         containerElement.classList.add('container2');
     } else {
         containerElement.classList.add('container1');
     }
 
-    const successpasteLabel = document.getElementById('successPasteLabel');
+    const successPasteLabel = document.getElementById('successPasteLabel');
     const welcomeLabel = document.getElementById('welcomeLabel');
     if (action === 'paste') {
-        successpasteLabel.setAttribute('style', 'display: block');
+        successPasteLabel.setAttribute('style', 'display: block');
     } else {
-        successpasteLabel.setAttribute('style', 'display: none');
+        successPasteLabel.setAttribute('style', 'display: none');
     }
-    if (action === 'copy' || aviCookieData) {
+    if (action === 'copy' || copyCookieData) {
         welcomeLabel.setAttribute('style', 'display: none');
     } else if (action === 'reset') {
         welcomeLabel.setAttribute('style', 'display: block');
@@ -101,7 +115,7 @@ const handlePopupUI = action => {
 };
 
 // When the popup HTML has loaded
-window.addEventListener('load', event => {
+window.addEventListener('load', () => {
     handlePopupUI();
 
     document
