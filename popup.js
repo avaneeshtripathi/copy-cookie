bade750f08621889fb76f2fd12b6f220c6edcb9a
Avaneesh Tripathi <avaneeshtripathi93@gmail.com>
2022-10-06 12:05:52 +0400

    chore: improvements and upgrade to manifest v3

--- popup.js
+++ popup.js
@@ -8,25 +8,42 @@ const onCopyButtonClick = () => {
         },
         tab => {
             chrome.cookies.getAll({ url: tab[0].url }, cookie => {
-                localStorage.aviCookieData = JSON.stringify(cookie);
+                localStorage.copyCookieData = JSON.stringify(cookie);
+                setTimeout(() => handlePopupUI('copy'), 100);
             });
         },
     );
-    setTimeout(() => handlePopupUI('copy'), 200);
+};
+
+const removeOldCookies = (cookies, index, url, callback) => {
+    if (!cookies[index]) return callback();
+
+    try {
+        chrome.cookies.remove({
+            url: url + cookies[index].path,
+            name: cookies[index].name,
+        }, () => removeOldCookies(cookies, index + 1, url, callback));
+    } catch (e) {
+        console.error(`There was an error removing the cookies: ${error}`, cookies[index].name);
+    }
 };
 
 // When the popup Paste Button is clicked
-const onPasteButtonClick = () => {
-    const aviCookieData = localStorage.aviCookieData
-        ? JSON.parse(localStorage.aviCookieData)
-        : null;
-    if (!aviCookieData)
-        return alert('Oh Man! You need to copy the cookies first.');
+const onPasteButtonClick = async () => {
+    let copyCookieData;
+    try {
+        copyCookieData = localStorage.copyCookieData
+            ? JSON.parse(localStorage.copyCookieData)
+            : null;
+    } catch (e) {
+        return alert('Error parsing cookies. Please try again.');
+    }
+
+    if (!copyCookieData)
+        return alert('Uh-Oh! You need to copy the cookies first.');
 
     let domain = document.getElementById('domainInput').value.trim();
     if (!domain) domain = 'localhost';
-
-    let validTabUrl = true;
 
     chrome.tabs.query(
         {
@@ -35,65 +52,57 @@ const onPasteButtonClick = () => {
             active: true,
         },
         tab => {
-            if (tab[0].url) {
-                chrome.cookies.getAll({ url: tab[0].url }, cookies => {
-                    for (let i = 0; i < cookies.length; i++) {
-                        chrome.cookies.remove({
-                            url: tab[0].url + cookies[i].path,
-                            name: cookies[i].name,
-                        });
-                    }
-                });
-                aviCookieData.forEach(({ name, value, path }, index) => {
-                    try {
-                        chrome.cookies.set({
-                            url: tab[0].url,
-                            name,
-                            value,
-                            path,
-                            domain,
-                        });
-                    } catch (error) {
-                        console.error(`There was an error: ${error}`);
-                    }
-                });
-            } else {
-                validTabUrl = false;
-                return alert('Tab with invalid URL. Are you kidding me ???');
+            if (!tab?.[0]?.url) {
+                return alert('Uh-Oh! Tab with invalid URL.');
             }
+
+            chrome.cookies.getAll({ url: tab[0].url }, cookies => {
+                removeOldCookies(cookies, 0, tab[0].url, () => {
+                    copyCookieData.forEach(({ name, value, path }) => {
+                        try {
+                            chrome.cookies.set({
+                                url: tab[0].url,
+                                name,
+                                value,
+                                path,
+                                domain,
+                            });
+                        } catch (error) {
+                            console.error(`There was an error: ${error}`);
+                        }
+                    });
+                    onResetButtonClick('paste');
+                });
+            });
         },
     );
-    setTimeout(() => {
-        if (validTabUrl) onResetButtonClick('paste');
-    }, 200);
 };
 
 // When the popup Reset Button is clicked
 const onResetButtonClick = action => {
-    localStorage.removeItem('aviCookieData');
+    localStorage.removeItem('copyCookieData');
     handlePopupUI(action);
 };
 
 const handlePopupUI = action => {
-    const aviCookieData = localStorage.aviCookieData
-        ? JSON.parse(localStorage.aviCookieData)
-        : null;
+    const copyCookieData = localStorage.copyCookieData;
     const containerElement = document.getElementById('container');
     containerElement.setAttribute('class', '');
-    if (aviCookieData) {
+
+    if (copyCookieData) {
         containerElement.classList.add('container2');
     } else {
         containerElement.classList.add('container1');
     }
 
-    const successpasteLabel = document.getElementById('successPasteLabel');
+    const successPasteLabel = document.getElementById('successPasteLabel');
     const welcomeLabel = document.getElementById('welcomeLabel');
     if (action === 'paste') {
-        successpasteLabel.setAttribute('style', 'display: block');
+        successPasteLabel.setAttribute('style', 'display: block');
     } else {
-        successpasteLabel.setAttribute('style', 'display: none');
+        successPasteLabel.setAttribute('style', 'display: none');
     }
-    if (action === 'copy' || aviCookieData) {
+    if (action === 'copy' || copyCookieData) {
         welcomeLabel.setAttribute('style', 'display: none');
     } else if (action === 'reset') {
         welcomeLabel.setAttribute('style', 'display: block');
@@ -101,7 +110,7 @@ const handlePopupUI = action => {
 };
 
 // When the popup HTML has loaded
-window.addEventListener('load', event => {
+window.addEventListener('load', () => {
     handlePopupUI();
 
     document
